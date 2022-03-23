177af166ab25d75f1c82ea3bb8f6beb6e8cc1477
Olga Shiryaeva <oshiryaeva@protonmail.com>
2022-03-23 13:35:05 -0500

    String repeat n times

---
+++ easy_codewars/string_repeat.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	// "strings"
+)
+
+func RepeatStr(repetitions int, value string) string {
+  // return strings.Repeat(value, repetitions)
+	// or
+  var result string = ""
+	for index := 0; index < repetitions; index++ {
+    result += value
+	}
+	return result
+}
+
+func main() {
+
+}
