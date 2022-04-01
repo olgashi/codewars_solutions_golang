ed5802c0af5710382277f1b520ac554c400c465d
Olga Shiryaeva <oshiryaeva@protonmail.com>
2022-04-01 14:23:17 -0500

    Make the deadfish swim

---
+++ easy_codewars/make_the_deadfish_swim.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func Parse(data string) []int {
+
+	resultSlice := []int{}
+	value := 0
+	dataList := strings.Split(data, "")
+
+	for _, el := range dataList {
+		switch el {
+		case "i":
+			value += 1
+		case "d":
+			value -= 1
+		case "s":
+			value *= value
+		case "o":
+			resultSlice = append(resultSlice, value)
+		default:
+			continue
+		}
+	}
+	return resultSlice
+}
+
+func main() {
+	fmt.Println(Parse("iiisdoso"))
+
+}
