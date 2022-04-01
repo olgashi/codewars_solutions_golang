df3f8faab1d034af3d77600e97eabd09c4dc0eff
Olga Shiryaeva <oshiryaeva@protonmail.com>
2022-03-31 20:08:41 -0500

    Remove comments

--- medium_codewars/valid_parentheses.go
+++ medium_codewars/valid_parentheses.go
@@ -25,58 +25,12 @@ func ValidParentheses(parens string) bool {
 }
 
 func main() {
-
+	fmt.Println("()") // true
+	fmt.Println(")(()))") // false
+	fmt.Println("(") // false 
+	fmt.Println("(())((()())())") // true 
+	fmt.Println("()()(())") // true
+	fmt.Println("") // false
+	fmt.Println(")))") // false
+	fmt.Println("(()))(") // false
 }
-
-/*
-PEDAC
-
-Problem
--------
-
-Inputs: 
-string of parentheses (length of between 0 and 100) - "(" or ")"
-
-Output:
-boolean indicating if string of parentheses is valid
-
-- valid string of parentheses - a string where each paren has a closing companion, and in correct order
-for example:, "()()", "(())()()(())"
-- invalid: "()(())(", ")("
-
-Examples
--------
-
-fmt.Println("()") // true
-fmt.Println(")(()))") // false
-fmt.Println("(") // false 
-fmt.Println("(())((()())())") // true 
-fmt.Println("()()(())") // true
-fmt.Println("") // false
-fmt.Println(")))") // false
-fmt.Println("(()))(") // false
-
-count
-
-DS
--------
-none
-
-Algorithm
--------
-// ( ) ( ) ( ( ) )
-
-create a variable parensCount, set to 0
-
-loop for index from 0 til the end of the string 
-  if parensCount < 0 
-	  return false
-
-return parensCount == 0
-
-Code
--------
-
-
-
-*/
