fa7bb32cc31ab990e1151e7bde7a4fe31a881c1a
Olga Shiryaeva <oshiryaeva@protonmail.com>
2022-04-04 13:07:59 -0500

    Weird Case

---
+++ easy_codewars/weird_string_cse.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func toWeirdCase(str string) (outputStr string) {
+
+	for _, word := range strings.Split(str, " ") {
+		for letterIndex, letter := range strings.Split(word, "") {
+			if letterIndex == 0 || letterIndex % 2 == 0 {
+				outputStr += strings.ToUpper(letter)
+			} else {
+				outputStr += strings.ToLower(letter)
+			}
+		}
+
+	outputStr += " "
+
+	}
+	return strings.TrimRight(outputStr, " ")
+}
+
+func main() {
+	fmt.Println(toWeirdCase("ABC")) // => returns "AbC"
+	// fmt.Println(toWeirdCase("String")) // => returns "StRiNg"
+  // fmt.Println(toWeirdCase("Weird string case")) // => returns "WeIrD StRiNg CaSe"
+}
