8825fd7da09a83d557decc6c140bf9bddd0e483a
Olga Shiryaeva <oshiryaeva@protonmail.com>
2022-04-04 14:29:18 -0500

    Cleanup

--- easy_codewars/highest_and_lowest.go
+++ easy_codewars/highest_and_lowest.go
@@ -6,41 +6,6 @@ import (
 	"strconv"
 )
 
-// PEDAC
-
-// Problem
-// Input: a string representing a list of space separated numbers
-// Outputs: string representing highest and lowest numbers
-
-// all numbers are confirmed to be valid int32
-// there will be at least 1 number in string
-// output must be 2 numbers with highest number first
-// can have negative numbers
-
-// Examples
-
-
-
-// DS
-// slice - convert input string into slice (list)
-
-// Algorithm
-// split input string using space as the delimiter => slice - inputNumList
-// create variable min, set to pos Infinity -3
-// create variable max, set to neg Infinity 5
-
-// iterate over inputNumList, for each iteration:
-// if int(current Number) < min
-//  set min to currentNumber
-// if int(current Number) > max
-//  set max to current Number
-
-// join max and min on space and return the resulting string
-
-// Code
-
-// fmt.Println(HighAndLow("1 2 -3 4 5")) // "5 -3"
-
 func HighAndLow(in string) string {
 	
 	inputNumList := strings.Split(in, " ")
