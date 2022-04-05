a85c159636e6520a6c4ba7d1ae5a5d70efb8a05a
Olga Shiryaeva <oshiryaeva@protonmail.com>
2022-04-05 12:45:05 -0500

    Cleanup

--- easy_codewars/growing_plant.go
+++ easy_codewars/growing_plant.go
@@ -4,41 +4,6 @@ import (
 	"fmt"
 )
 
-// PEDAC
-/*
-- each day plant grows by upSpeed meters, 5 <= upSpeed <= 100 
-- each night plant decreses by downSpeed meters, 2 <= downSpeed < upSpeed
-- initally the plant is 0 meters
-- seed is planted at the beginning of day
-- objective is to find a day when plant reaches a certain level
-- desiredHeight - 4 <= desiredHeight <= 1000, represents treshold
-- output - integer representing number of days will take for the plant to reach desiredHeight
-
-// DS
-none
-
-// GrowingPlant(5, 2, 6)) // 2
-upSpeed = 5 |
-downSpeed = 2 |
-desiredHeight = 6 |
-growingPlantHeight = 0 + 5 - 2 + 5
-numDays = 0 + 1 + 1
-
-// Algorithm
-
-create variable growingPlantHeight, set it to 0
-create variable numDays = 0
-
-loop while true
-  growingPlantHeight += upSpeed
-	numDays +=1
-	if growingPlantHeight >= desiredHeight
-	  return numDays
-	
-	growingPlantHeight -= downSpeed
-
-*/
-
 func GrowingPlant(upSpeed, downSpeed, desiredHeight int) (numDays int) {
 	growingPlantHeight := 0
 
