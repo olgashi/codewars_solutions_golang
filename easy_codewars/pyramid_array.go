d5ff99049d80c0167f1ef850821194efb7800482
Olga Shiryaeva <oshiryaeva@protonmail.com>
2022-04-05 10:21:54 -0500

    Cleanup

--- easy_codewars/pyramid_array.go
+++ easy_codewars/pyramid_array.go
@@ -19,33 +19,8 @@ func Pyramid(n int) [][]int {
 
 func main() {
 	fmt.Println(Pyramid(0)) // []
-	// fmt.Println(Pyramid(1)) // [[1]]
-	// fmt.Println(Pyramid(2)) // [[1],[1,1]]
-	// fmt.Println(Pyramid(3)) // [[1],[1,1],[1,1,1]]
+	fmt.Println(Pyramid(1)) // [[1]]
+	fmt.Println(Pyramid(2)) // [[1],[1,1]]
+	fmt.Println(Pyramid(3)) // [[1],[1,1],[1,1,1]]
 	fmt.Println(Pyramid(4)) // [[1],[1,1],[1,1,1][1,1,1,1]]
 }
-
-/*
-PEDAC
-Input: integer n >=0, representing number of sub arrays and range of length betyween the first sub array and last subarray
-Output: nested array of length n
-
-- all sub arrays should be filled with 1s
-- each subarray is filled with elements 
-- if n is 0, no subarrays are added
-
-
-DS: arrays (nested)
-// fmt.Println(Pyramid(3)) // [[1],[1,1],[1,1,1]]
-
-i= 3
-
-Algorithm:
-- create an array of size n, set to empty array - outputArray
-
-- loop for i..1 to <= n, at each iteration:
-  create an array of size i and fill it with i number of 1s
-	append to output array
-
-return outputArray
-*/
