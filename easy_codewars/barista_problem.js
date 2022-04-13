9d5fdf63b3541dd651af02b3ed016d7a1ea66098
Olga Shiryaeva <oshiryaeva@protonmail.com>
2022-04-13 15:48:27 -0500

    Barista problem - I

---
+++ easy_codewars/barista_problem.js
@@ -0,0 +1,30 @@
+function barista(coffees) {
+  coffees.sort((a, b) => parseInt(a, 10) - parseInt(b, 10));
+  let sum = coffees[0];
+  let finalSum = sum || 0;
+  for (let index = 1; index < coffees.length; index++) {
+    sum += 2 + coffees[index];
+    finalSum += sum;
+  }
+  return finalSum;
+}
+
+console.log(barista([])) // 0
+console.log(barista([2, 10, 5, 3, 9])) // 85
+console.log(barista([4, 3, 2])) // 22
+console.log(barista([20, 5])) // 32
+console.log(barista([20,5,4,3,1,5,7,8])) // 211
+console.log(barista([5,4,3,2,1])) // 55
+
+/*
+2 3 5 9 10
+
+2
+2 + 2 + 3 = 7
+7 + 2 + 5 = 14
+14 + 2 + 9 = 25
+25 + 2 + 10 = 37
+
+
+
+*/
